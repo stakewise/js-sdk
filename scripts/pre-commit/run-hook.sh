57bac15f830d55867388894716575569661a2f86
MikeDiam <info@mdiamond.ru>
2022-02-14 18:59:42 +0300

    [methods-package] Update pre-commit hook

--- scripts/pre-commit/run-hook.sh
+++ scripts/pre-commit/run-hook.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Running pre-commit hook"
-./scripts/pre-commit/run-tests.sh
-
 RED='\033[0;31m'
 NC='\033[0m'
+
+echo "Running pre-commit hook"
+./scripts/pre-commit/run-tests.sh
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
