065c0760d82bb591a12392dda80e50473428cf31
MikeDiam <info@mdiamond.ru>
2022-02-15 17:13:50 +0300

    [methods-package] Update pre-commit hook

---
+++ packages/stakewise-methods/src/index.test.ts
@@ -0,0 +1,20 @@
+import crypto from 'crypto'
+import faker from '@faker-js/faker'
+import { Web3Provider } from '@ethersproject/providers'
+
+import Methods from './index'
+
+
+const string = faker.random.word()
+const address = `0x${crypto.randomBytes(32).toString('hex').slice(0, 40)}`
+
+describe('index.ts', () => {
+
+  it('creates instance of Methods with getBalances method', () => {
+    const methods = new Methods({
+      address,
+      referral: string,
+      provider: new Web3Provider('WalletLink'),
+    })
+  })
+})
