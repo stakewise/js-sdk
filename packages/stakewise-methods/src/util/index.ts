bfa8334687c4e1e88ac9a0474af9e5b18807ea4b
MikeDiam <info@mdiamond.ru>
2022-02-18 14:48:33 +0300

    [methods-package-get-apr] Update tests

--- packages/stakewise-methods/src/util/index.ts
+++ packages/stakewise-methods/src/util/index.ts
@@ -7,3 +7,4 @@ export { default as createContracts } from './createContracts'
 export type { Contracts } from './createContracts'
 
 export { default as fetchPoolStats } from './fetchPoolStats'
+export type { FetchPoolStatsResult } from './fetchPoolStats'
