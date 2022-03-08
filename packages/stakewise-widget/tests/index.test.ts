018e55ba7392dce87be13a5a162e35fb9cb52a27
MikeDiam <info@mdiamond.ru>
2022-03-08 16:49:26 +0300

    [widget-package] Add widget init tests

--- packages/stakewise-widget/tests/index.test.ts
+++ packages/stakewise-widget/tests/index.test.ts
@@ -1,9 +1,48 @@
 import Widget from '../src/index'
 
+const { ethers } = require('hardhat')
+
+
+let address: string
+let referral: string
+
+const createWidget = () => (
+  new Widget({
+    provider: ethers.provider,
+    address,
+    referral,
+  })
+)
 
 describe('src/index.ts', () => {
 
-  it.skip('creates Widget', () => {
+  beforeAll(async () => {
+    const [ account1, account2 ] = await ethers.getSigners()
 
+    address = account1.address
+    referral = account2.address
+  })
+
+  it('creates Widget', () => {
+    global.document = {
+      body: {
+        // @ts-ignore
+        attachShadow: () => {},
+      },
+    }
+
+    const widget = createWidget()
+
+    expect(typeof widget.open).toEqual('function')
+    expect(typeof widget.close).toEqual('function')
+  })
+
+  it('throws an error if attachShadow is not supported', () => {
+    global.document = {
+      // @ts-ignore
+      body: {},
+    }
+
+    expect(() => createWidget()).toThrowError(/Current browser is not supported/)
   })
 })
