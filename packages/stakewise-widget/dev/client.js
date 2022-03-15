ae05377c2d1fcfeed910b9ff129d70dedb0c0b84
MikeDiam <info@mdiamond.ru>
2022-03-15 14:01:10 +0300

    [widget-package] Update modal

--- packages/stakewise-widget/dev/client.js
+++ packages/stakewise-widget/dev/client.js
@@ -2,16 +2,22 @@ import { providers } from 'ethers'
 import Widget from '../dist'
 
 
-const widget = new Widget({
-  address: '0xEd5dBc418eB6b7Cb330f0df8fdb50a8772b8C4d0',
-  referral: '0xEd5dBc418eB6b7Cb330f0df8fdb50a8772b8C4d0',
-  provider: new providers.Web3Provider(web3.currentProvider),
-  onError: (data) => {
-    console.log('error', data)
-  }
-})
+const button = document.createElement('button')
 
-widget.open()
+button.innerHTML = 'Open modal'
+button.onclick = () => {
+  const widget = new Widget({
+    address: '0xEd5dBc418eB6b7Cb330f0df8fdb50a8772b8C4d0',
+    referral: '0xEd5dBc418eB6b7Cb330f0df8fdb50a8772b8C4d0',
+    provider: new providers.Web3Provider(window.web3?.currentProvider),
+    onError: (data) => {
+      console.log('error', data)
+    }
+  })
 
+  widget.open()
+}
 
-export default widget
+document.body.appendChild(button)
+
+// widget.open()
--- packages/stakewise-widget/src/index.ts
+++ packages/stakewise-widget/src/index.ts
@@ -1,19 +1,27 @@
 import WidgetType, { Options, OpenProps } from 'stakewise-widget'
-import Methods from 'stakewise-methods'
+import Methods, { GetBalancesResult } from 'stakewise-methods'
 
 import { validateBrowser, validateOptions } from './util'
+
+// import './styles.css'
 
 
 class Widget implements WidgetType {
 
   private methods: Methods
-  private container: HTMLElement | null = null
+
+  private rootContainer: HTMLElement
+  private shadowRoot: DocumentFragment
+
+  private content: HTMLElement | null = null
+  private overlay: HTMLElement
+
   private status: 'loading' | 'error' | 'initial' = 'loading'
-  private initialView: {
-    status: 'loading' | 'success' | 'default'
-  } = {
-    status: 'default'
-  }
+  // private initialView: {
+  //   status: 'loading' | 'success' | 'default'
+  // } = {
+  //   status: 'default'
+  // }
 
   private callbacks: {
     onSuccess?: Options['onSuccess']
@@ -33,22 +41,49 @@ class Widget implements WidgetType {
       onError,
       onClose,
     }
+
+    this.rootContainer = document.createElement('div')
+    this.shadowRoot = this.rootContainer.attachShadow({ mode: 'closed' })
+
+    const stylesLink = document.createElement('link')
+    stylesLink.rel = 'stylesheet'
+    stylesLink.href = './styles.css'
+
+    this.shadowRoot.appendChild(stylesLink)
+    document.body.appendChild(this.rootContainer)
+
+    this.overlay = document.createElement('div')
+    this.overlay.classList.add('overlay')
+  }
+
+  private renderModal() {
+    this.overlay.innerHTML = `
+      <div id="modal">
+        <div id="content">
+          <div class="loader"></div>
+        </div>
+        <button id="close"></button>
+      </div>
+    `
+
+    this.content = this.shadowRoot.getElementById('content')
+    const modal = this.shadowRoot.getElementById('modal') as HTMLElement
+    const closeButton = this.shadowRoot.getElementById('close') as HTMLElement
+
+    this.overlay.onclick = () => this.handleClose()
+    closeButton.onclick = () => this.handleClose()
+    modal.onclick = (event) => event.stopPropagation()
   }
 
   private renderError() {
-    if (this.container && this.status !== 'error') {
+    if (this.content && this.status !== 'error') {
       this.status = 'error'
-    }
-  }
-
-  private async renderLoading() {
-    if (this.container && this.status !== 'loading') {
-      this.status = 'loading'
+      this.content.innerHTML = '<div>Error</div>'
     }
   }
 
   private setLoading(isEnabled: boolean) {
-    if (this.container && this.status === 'initial') {
+    if (this.content && this.status === 'initial') {
       if (isEnabled) {
         // set loading button
       }
@@ -58,9 +93,11 @@ class Widget implements WidgetType {
     }
   }
 
-  private async renderInitial() {
-    if (this.container && this.status !== 'initial') {
+  private async renderInitial({ balances, stakingApr }: { balances: GetBalancesResult, stakingApr: number }) {
+    if (this.content && this.status !== 'initial') {
       this.status = 'initial'
+
+      this.content.innerHTML = '<div>Initial</div>'
 
       // onButtonClick
       // callMethod(this.methods.deposit)
@@ -75,8 +112,6 @@ class Widget implements WidgetType {
           error,
         })
 
-        this.renderError()
-
         return Promise.reject(error)
       })
   }
@@ -86,27 +121,25 @@ class Widget implements WidgetType {
       this.callMethod(() => this.methods.getBalances(), 'getBalances'),
       this.callMethod(() => this.methods.getStakingApr(), 'getStakingApr'),
     ])
-      .then(([ balances, stakingApr ]) => {
-        console.log({ balances, stakingApr })
-      })
+      .then(([ balances, stakingApr ]) => ({ balances, stakingApr }))
   }
 
   open(props: OpenProps) {
-    if (!this.container) {
-      this.container = document.createElement('div')
-      this.container.attachShadow({ mode: 'closed' })
-      document.body.appendChild(this.container)
+    this.shadowRoot.appendChild(this.overlay)
 
-      this.renderLoading()
-      this.fetchInitial()
-        .then(() => this.renderInitial())
-    }
+    this.renderModal()
+    this.fetchInitial()
+      .then(({ balances, stakingApr }) => this.renderInitial({ balances, stakingApr }))
+      .catch(() => this.renderError())
+  }
+
+  private handleClose() {
+    this.shadowRoot.removeChild(this.overlay)
   }
 
   close() {
-    if (this.container) {
-      document.body.removeChild(this.container)
-      this.container = null
+    if (this.rootContainer) {
+      this.handleClose()
 
       if (this.callbacks.onClose) {
         this.callbacks.onClose()
